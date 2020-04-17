f2647f1d54f1ec0a72e74259a5a4f6e9cbbb4dea
guohaoyu110 <guohaoyu110@gmail.com>
2020-04-17 15:41:08 -0700

    update backend

--- backend/server.js
+++ backend/server.js
@@ -1,56 +1,66 @@
-"use strict";
+// "use strict";
 const express = require("express");
 const app = express();
-const cors = require("cors");
-app.use(express.static("static"));
-app.use(express.static('public'));
+var fetch = require("node-fetch");
+let fs = require('fs');
 
-app.use(cors());
 
 let port = 6000;
 
-function getstates(){
-    const { NovelCovid } = require('novelcovid');
-    const track = new NovelCovid();
-    let statedata = track.states();
+async function getstates(){
+    // const { NovelCovid } = require('novelcovid');
+    // // const track = new NovelCovid();
+    // let statedata = track.states();
 
-    statedata.then(function(result) {
-        //console.log(result); // "Some User token"
-        let data = JSON.stringify(result);
-        //console.log(data);
-        let fs = require('fs');
-        fs.writeFile('public/state.json', data, () => {});
+    // statedata.then(function(result) {
+    //     //console.log(result); // "Some User token"
+    //     let data = JSON.stringify(result);
+    //     //console.log(data);
+    //     let fs = require('fs');
+    //     fs.writeFile('state.json', data, () => {});
+    // });
+    const state_url = "https://corona.lmao.ninja/v2/states";
+    // const response = await fetch(state_url);
+    // const data = await response.json();
+    // console.log('data: ', data);
+    fetch(state_url).then(function(response) {
+        return response.json();
+    }).then(function (data){
+        console.log(data);
+        fs.writeFile('state.json',response.json(),()=>{});
+    }).catch(function(e){
+        console.log("Oops, error");
     });
+    
 }
 
-function getcountries(){
-    const { NovelCovid } = require('novelcovid');
-    const track = new NovelCovid();
-    let countrydata = track.countries();
+// async function getcountries(){
+//     const { NovelCovid } = require('novelcovid');
+//     const track = new NovelCovid();
+//     let countrydata = track.countries();
 
-    countrydata.then(function(result) {
-        //console.log(result); // "Some User token"
-        result.forEach((obj) => { delete obj.countryInfo; });
-        let data = JSON.stringify(result);
-        //console.log(data);
-        let fs = require('fs');
-        fs.writeFile('public/country.json', data, () => {});
-    });
-}
+//     countrydata.then(function(result) {
+//         //console.log(result); // "Some User token"
+//         result.forEach((obj) => { delete obj.countryInfo; });
+//         let data = JSON.stringify(result);
+//         //console.log(data);
+//         let fs = require('fs');
+//         fs.writeFile('country.json', data, () => {});
+//     });
+// }
 
 async function submit(){
     getstates();
-    getcountries();
+    // getcountries();
     let update = setInterval(() => {
         getstates();
-        getcountries();
+        // getcountries();
     }, 3600000); // update it every hour
 }
 
 submit();
 
-// app.listen(port, err => {
-//     console.log(`Listening on port: ${port}`);
+app.listen(port, err => {
+    console.log(`Listening on port: ${port}`);
     
-// })
-app.listen(port, () => console.log(`Listening on port: ${port}`);)
+})
