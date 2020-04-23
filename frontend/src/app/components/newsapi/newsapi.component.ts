a6de2f0ec64a45681d3dba1053a97675dc4c70a9
guohaoyu110 <guohaoyu110@gmail.com>
2020-04-23 16:07:20 -0700

    update newapi module

--- frontend/src/app/components/newsapi/newsapi.component.ts
+++ frontend/src/app/components/newsapi/newsapi.component.ts
@@ -12,8 +12,10 @@ export class NewsapiComponent implements OnInit {
   newsData : news[] = [];
   constructor(private http: HttpClient) { }
   getLatestNews(){
-    const url = "https://api.cognitive.microsoft.com/bing/v7.0/news/search?q=coronavirus";
+    const url = "https://api.cognitive.microsoft.com/bing/v7.0/news/search?q=uscoronavirus";
+    // var url = "https://gnews.io/api/v3/search?q="+"&token=d02959da05c216ca2a05811eff49b7e7"
     return this.http.get(url, {headers : {'Ocp-Apim-Subscription-Key' : '9fc2b0698a444b7c9af64925d0447951'  }});
+    // return this.http.get(url);
     
   }
   ngOnInit(): void {
