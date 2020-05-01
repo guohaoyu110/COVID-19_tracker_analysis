fd8f849bc3459975eeedb1871b9cb4fc38c0f390
njuxty <32052144+njuxty@users.noreply.github.com>
2020-05-01 00:40:45 -0700

    Update test.js

--- backend/test.js
+++ backend/test.js
@@ -15,3 +15,27 @@ describe("isEmail Test with true case", () => {
       assert.equal(res, true);
     });
 });
+
+// Testing getBase
+describe("getBase Test", () => {
+    it("should get base data", () => {
+      myServer.getBase();
+      assert.notEqual(myServer.baseinfo, null);
+    });
+});
+
+// Testing getStates
+describe("getStates Test", () => {
+    it("should get state data", () => {
+      myServer.getStates();
+      assert.notEqual(myServer.stateinfo, null);
+    });
+});
+
+// Testing getCountries
+describe("getCountries Test", () => {
+    it("should get country data", () => {
+      myServer.getCountries();
+      assert.notEqual(myServer.countryinfo, null);
+    });
+});
